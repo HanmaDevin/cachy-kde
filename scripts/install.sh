a178b0f499d4dbc7b54eef92691a6f23661047c9
HanmaDevin <Hanma@Devin.com>
2025-10-25 15:17:38 +0200

    added rust analyzer for lsp in neovim

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,34 +1,34 @@
 #! /bin/bash
-#    ____           __        ____   _____           _       __ 
+#    ____           __        ____   _____           _       __
 #    /  _/___  _____/ /_____ _/ / /  / ___/__________(_)___  / /_
 #    / // __ \/ ___/ __/ __ `/ / /   \__ \/ ___/ ___/ / __ \/ __/
-#  _/ // / / (__  ) /_/ /_/ / / /   ___/ / /__/ /  / / /_/ / /_  
-# /___/_/ /_/____/\__/\__,_/_/_/   /____/\___/_/  /_/ .___/\__/  
-#                                                  /_/           
+#  _/ // / / (__  ) /_/ /_/ / / /   ___/ / /__/ /  / / /_/ / /_
+# /___/_/ /_/____/\__/\__,_/_/_/   /____/\___/_/  /_/ .___/\__/
+#                                                  /_/
 clear
 
 from="$HOME/cachy-kde"
 cfgPath="$from/.config"
 
 installPackages() {
-    local packages=("gum" "libreoffice" "rust-src" "mpv-mpris" "fastfetch" "glow" "ntfs-3g" "tree" "discord" "lazygit" "ufw" "zsh" "unzip" "wget" "yazi" "neovim" "eza" "btop" "gamemode" "steam" "mangohud" "zoxide" "fzf" "bat" "jdk-openjdk" "docker" "ripgrep" "cargo" "fd" "starship" "wine" "python-pip" "python-requests" "python-pipx" "openssh" "pam-u2f" "ttf-font-awesome" "ttf-nerd-fonts-symbols" "ttf-jetbrains-mono-nerd" "noto-fonts-emoji" "libfido2" "qt5-wayland" "qt6-wayland" "xdg-desktop-portal-gtk" "xdg-desktop-portal-wlr" "gdb" "pacman-contrib" "libimobiledevice" "usbmuxd" "gvfs-gphoto2" "ifuse" "python-dotenv" "openvpn" "ncdu" "texlive" "lynx" "inetutils" "net-tools" "jq" "nodejs" "npm" "github-cli" "protonmail-bridge" "proton-vpn-gtk-app" "wireguard-tools")
-    for pkg in "${packages[@]}"; do
-        sudo pacman -S --noconfirm "$pkg"
-    done
+  local packages=("gum" "libreoffice" "rust-src" "rust-analyzer" "mpv-mpris" "fastfetch" "glow" "ntfs-3g" "tree" "discord" "lazygit" "ufw" "zsh" "unzip" "wget" "yazi" "neovim" "eza" "btop" "gamemode" "steam" "mangohud" "zoxide" "fzf" "bat" "jdk-openjdk" "docker" "ripgrep" "cargo" "fd" "starship" "wine" "python-pip" "python-requests" "python-pipx" "openssh" "pam-u2f" "ttf-font-awesome" "ttf-nerd-fonts-symbols" "ttf-jetbrains-mono-nerd" "noto-fonts-emoji" "libfido2" "qt5-wayland" "qt6-wayland" "xdg-desktop-portal-gtk" "xdg-desktop-portal-wlr" "gdb" "pacman-contrib" "libimobiledevice" "usbmuxd" "gvfs-gphoto2" "ifuse" "python-dotenv" "openvpn" "ncdu" "texlive" "lynx" "inetutils" "net-tools" "jq" "nodejs" "npm" "github-cli" "protonmail-bridge" "proton-vpn-gtk-app" "wireguard-tools")
+  for pkg in "${packages[@]}"; do
+    sudo pacman -S --noconfirm "$pkg"
+  done
 }
 
 installAurPackages() {
-    local packages=("google-chrome" "openvpn-update-systemd-resolved" "lazydocker" "qt-heif-image-plugin" "luajit-tiktoken-bin" "ani-cli" "konsave")
-    for pkg in "${packages[@]}"; do
-        yay -S --noconfirm "$pkg"
-    done
+  local packages=("google-chrome" "openvpn-update-systemd-resolved" "lazydocker" "qt-heif-image-plugin" "luajit-tiktoken-bin" "ani-cli" "konsave")
+  for pkg in "${packages[@]}"; do
+    yay -S --noconfirm "$pkg"
+  done
 }
 
 installYay() {
-    git clone https://aur.archlinux.org/yay.git "$HOME/yay"
-    cd "$HOME/yay"
-    makepkg -si
-    echo ":: yay has been installed successfully."
+  git clone https://aur.archlinux.org/yay.git "$HOME/yay"
+  cd "$HOME/yay"
+  makepkg -si
+  echo ":: yay has been installed successfully."
 }
 
 installDeepCoolDriver() {
@@ -93,15 +93,15 @@ detect_nvidia() {
 copy_config() {
   gum spin --spinner dot --title "Creating bakups..." -- sleep 2
 
-  if [[ -f "$HOME/.zshrc" ]]; then 
+  if [[ -f "$HOME/.zshrc" ]]; then
     mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
   fi
 
-  if [[ -d "$HOME/.config" ]]; then 
+  if [[ -d "$HOME/.config" ]]; then
     mv "$HOME/.config" "$HOME/.config.bak"
   fi
 
-  if [[ ! -d "$HOME/Pictures/Screenshots/" ]]; then 
+  if [[ ! -d "$HOME/Pictures/Screenshots/" ]]; then
     mkdir -p "$HOME/Pictures/Screenshots/"
   fi
 
@@ -112,11 +112,11 @@ copy_config() {
   sudo cp -r "$from/Cursor/Bibata-Modern-Ice" "/usr/share/icons"
   sudo cp -r "$from/fonts/" "/usr/share"
   sudo cp -r "$from/icons/" "/usr/share/"
- 
+
   echo "Want to install Vencord?"
   vencord=$(gum choose "Yes" "No")
 
-  if [[ "$vencord" == "Yes" ]]; then 
+  if [[ "$vencord" == "Yes" ]]; then
     bash "$from/Vencord/VencordInstaller.sh"
     cp -r "$from/Vencord/themes/" "$HOME/.config/Vencord/"
   fi
@@ -138,21 +138,21 @@ EOF
 echo "CachyOS KDE Setup"
 echo -e "${NONE}"
 while true; do
-    read -r -p "Do you want to start the installation now? (Yy/Nn): " yn
-    case $yn in
-        [Yy]*)
-            echo ":: Installation started."
-            echo
-            break
-            ;;
-        [Nn]*)
-            echo ":: Installation canceled"
-            exit
-            ;;
-        *)
-            echo ":: Please answer yes or no."
-            ;;
-    esac
+  read -r -p "Do you want to start the installation now? (Yy/Nn): " yn
+  case $yn in
+  [Yy]*)
+    echo ":: Installation started."
+    echo
+    break
+    ;;
+  [Nn]*)
+    echo ":: Installation canceled"
+    exit
+    ;;
+  *)
+    echo ":: Please answer yes or no."
+    ;;
+  esac
 done
 
 sudo pacman -Syu
